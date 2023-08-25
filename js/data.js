b7b421ddfa3a89e2913491f505f6fa83031fdaaa
Adnane Abid <92402125+adnane84@users.noreply.github.com>
2023-08-25 17:08:32 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,43 +1,172 @@
-
 export const data = [
   {
-    "productName": "Smartphone",
-    "price": 399.99,
-    "brand": "TechCo",
-    "color": "Black",
+    "productName": "Moroccan Leather Pouf",
+    "price": 129.99,
+    "craftsperson": "Leather Artisan",
+    "color": "Cognac",
     "availability": true,
-    "image": "https://example.com/smartphone.jpg"
+    "image": "./assets/MoroccanLeatherPouf.jpg",
+    "description": "Add a touch of Moroccan luxury to your home with this handcrafted leather pouf. Skillfully created by a master leather artisan, the pouf features intricate tooling and a rich cognac hue. Perfect as extra seating or as a stylish accent piece."
   },
   {
-    "productName": "Laptop",
-    "price": 999.99,
-    "brand": "LaptopTech",
+    "productName": "Berber Handwoven Basket",
+    "price": 59.99,
+    "craftsperson": "Basket Weaver",
+    "color": "Natural",
+    "availability": true,
+    "image": "./assets/BerberHandwovenBasket.jpg",
+    "description": "Bring the spirit of the Moroccan souks to your space with this Berber handwoven basket. Meticulously crafted by a skilled basket weaver, it showcases traditional patterns and natural fibers. Ideal for storing blankets, magazines, or as a standalone decor item."
+  },
+  {
+    "productName": "Mosaic Tiled Mirror",
+    "price": 179.99,
+    "craftsperson": "Mosaic Artist",
+    "color": "Turquoise",
+    "availability": true,
+    "image": "./assets/MosaicTiledMirror.jpg",
+    "description": "Reflect Moroccan elegance in your home with this stunning mosaic tiled mirror. Handcrafted by a mosaic artist, it features intricate patterns in vibrant turquoise hues. A functional piece of art that adds a touch of exotic beauty to any room."
+  },
+  {
+    "productName": "Spice-Infused Tagine",
+    "price": 89.99,
+    "craftsperson": "Ceramics Expert",
+    "color": "Terracotta",
+    "availability": true,
+    "image": "./assets/Tagine.jpg",
+    "description": "Experience the flavors of Morocco with this spice-infused tagine. Expertly crafted by a ceramics artisan, it's made from authentic terracotta and features hand-painted motifs. Elevate your culinary adventures with this traditional Moroccan cookware."
+  },
+  {
+    "productName": "Atlas Mountains Rug",
+    "price": 299.99,
+    "craftsperson": "Rug Artisan",
+    "color": "Multicolored",
+    "availability": true,
+    "image": "./assets/MoroccanRug.jpg",
+    "description": "Wrap your space in the warmth of Morocco with this Atlas Mountains rug. Handwoven by a skilled rug artisan, it showcases a symphony of colors and traditional geometric patterns. Each step is a reminder of Morocco's breathtaking landscapes."
+  },
+  {
+    "productName": "Handcrafted Moroccan Lantern",
+    "price": 69.99,
+    "craftsperson": "Metalworker",
+    "color": "Antique Brass",
+    "availability": true,
+    "image": "./assets/MoroccanLantern.jpeg",
+    "description": "Illuminate your surroundings with the enchanting glow of this handcrafted Moroccan lantern. Meticulously shaped by a skilled metalworker, its intricate cutouts cast captivating patterns of light and shadow. An ode to Moroccan nights."
+  },
+  {
+    "productName": "Traditional Moroccan Tea Set",
+    "price": 119.99,
+    "craftsperson": "Ceramics Artisan",
+    "color": "Emerald Green",
+    "availability": true,
+    "image": "./assets/MoroccanTeaSet.jpg",
+    "description": "Savor the timeless tradition of Moroccan tea ceremonies with this exquisite tea set. Crafted by a ceramics artisan, it features intricate hand-painted patterns in vivid emerald green. Elevate your tea rituals with this cultural masterpiece."
+  },
+  {
+    "productName": "Majestic Moroccan Lamp",
+    "price": 199.99,
+    "craftsperson": "Lamp Artisan",
+    "color": "Gold",
+    "availability": true,
+    "image": "./assets/MoroccanLamp.jpeg",
+    "description": "Bask in the warm ambiance of this majestic Moroccan lamp. Handcrafted by a skilled lamp artisan, its intricate metalwork and golden finish cast captivating patterns when illuminated. Elevate your space with a touch of Moroccan mystique."
+  },
+  {
+    "productName": "Handwoven Moroccan Blanket",
+    "price": 79.99,
+    "craftsperson": "Weaver",
+    "color": "Amber",
+    "availability": true,
+    "image": "./assets/MoroccanBlanket.jpg",
+    "description": "Wrap yourself in the artistry of Morocco with this handwoven blanket. Meticulously crafted by a skilled weaver, it features captivating amber hues and traditional patterns. A cozy reminder of Moroccan craftsmanship."
+  },
+  {
+    "productName": "Artisanal Moroccan Soap",
+    "price": 12.99,
+    "craftsperson": "Soapmaker",
+    "color": "Cream",
+    "availability": true,
+    "image": "./assets/MoroccanSoap.jpg",
+    "description": "Indulge in the luxury of Moroccan skincare with this artisanal soap. Lovingly crafted by a soapmaker, it's enriched with traditional ingredients like argan oil and scented with exotic fragrances. Elevate your bathing routine to a sensory experience."
+  },
+  {
+    "productName": "Moroccan Ceramic Vase",
+    "price": 49.99,
+    "craftsperson": "Ceramics Artist",
+    "color": "Turquoise",
+    "availability": true,
+    "image": "./assets/MoroccanVase.jpeg",
+    "description": "Adorn your space with the beauty of Moroccan ceramics through this exquisite vase. Hand-shaped by a ceramics artist, its vibrant turquoise glaze and intricate motifs capture the essence of Moroccan artistry. A vessel of elegance and culture."
+  },
+  {
+    "productName": "Hand-Tooled Leather Journal",
+    "price": 29.99,
+    "craftsperson": "Leather Craftsman",
+    "color": "Chestnut",
+    "availability": true,
+    "image": "./assets/MoroccanJournal.jpeg",
+    "description": "Unleash your creativity on the pages of this hand-tooled leather journal. Crafted by a master leather craftsman, it features a rich chestnut color and intricate embossed designs. A companion for your thoughts and inspirations."
+  },
+  {
+    "productName": "Moroccan Ceramic Serving Tagine",
+    "price": 139.99,
+    "craftsperson": "Ceramics Artisan",
+    "color": "Deep Blue",
+    "availability": true,
+    "image": "./assets/MoroccanServingTagine.jpg",
+    "description": "Present your culinary creations in authentic Moroccan style with this ceramic serving tagine. Handcrafted by a skilled ceramics artisan, its deep blue hues and hand-painted patterns add a touch of Moroccan flair to your table."
+  }, {
+    "productName": "Moroccan Hammam Towel",
+    "price": 34.99,
+    "craftsperson": "Weaver",
+    "color": "Azure",
+    "availability": true,
+    "image": "./assets/MoroccanHammamTowel.jpeg",
+    "description": "Experience the luxury of Moroccan bath rituals with this authentic hammam towel. Handwoven by a skilled weaver, its azure tones and soft texture make it an essential companion for relaxation and self-care."
+  },
+  {
+    "productName": "Intricate Henna Art Tray",
+    "price": 89.99,
+    "craftsperson": "Henna Artist",
+    "color": "Brown",
+    "availability": true,
+    "image": "./assets/MoroccanHennaTray.jpg",
+    "description": "Add a touch of Moroccan henna art to your decor with this intricate tray. Crafted by a talented henna artist, its rich brown hues and delicate motifs are reminiscent of traditional designs. A captivating fusion of art and functionality."
+  },
+  {
+    "productName": "Moroccan Pottery Incense Burner",
+    "price": 24.99,
+    "craftsperson": "Potter",
+    "color": "Terracotta",
+    "availability": true,
+    "image": "./assets/MoroccanIncenseBurner.jpg",
+    "description": "Infuse your space with the scents of Morocco using this pottery incense burner. Handmade by a skilled potter, its terracotta body and intricate carvings create a captivating play of light and aroma."
+  },
+  {
+    "productName": "Engraved Moroccan Cuff Bracelet",
+    "price": 59.99,
+    "craftsperson": "Jewelry Artisan",
     "color": "Silver",
     "availability": true,
-    "image": "https://example.com/laptop.jpg"
+    "image": "./assets/MoroccanCuffBracelet.jpg",
+    "description": "Adorn yourself with the elegance of Morocco through this engraved cuff bracelet. Meticulously crafted by a jewelry artisan, its silver finish and intricate patterns add a touch of North African allure to your ensemble."
   },
   {
-    "productName": "Headphones",
-    "price": 79.99,
-    "brand": "AudioSounds",
-    "color": "White",
-    "availability": false,
-    "image": "https://example.com/headphones.jpg"
-  },
-  {
-    "productName": "Smartwatch",
-    "price": 149.99,
-    "brand": "WristTech",
-    "color": "Rose Gold",
+    "productName": "Handmade Moroccan Scented Candle",
+    "price": 19.99,
+    "craftsperson": "Candle Maker",
+    "color": "Amber",
     "availability": true,
-    "image": "https://example.com/smartwatch.jpg"
+    "image": "./assets/MoroccanScentedCandle.jpg",
+    "description": "Immerse your surroundings in the exotic scents of Morocco with this handmade scented candle. Created by a dedicated candle maker, its amber glow and captivating fragrance evoke the spirit of Moroccan bazaars."
   },
   {
-    "productName": "TV",
-    "price": 599.99,
-    "brand": "ScreenTech",
-    "color": "Black",
+    "productName": "Moroccan Leather Bound Book",
+    "price": 44.99,
+    "craftsperson": "Bookbinder",
+    "color": "Deep Red",
     "availability": true,
-    "image": "https://example.com/tv.jpg"
+    "image": "./assets/MoroccanLeatherBook.jpeg",
+    "description": "Embrace the allure of Moroccan literature and craftsmanship with this leather-bound book. Skillfully bound by a master bookbinder, its deep red cover and embossed designs make it a treasured keepsake."
   }
-]
+];
