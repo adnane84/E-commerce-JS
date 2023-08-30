739f946107d069b152650f8da758e30bc23adcfb
Adnane Abid <92402125+adnane84@users.noreply.github.com>
2023-08-30 17:06:58 -0400

    Update sorting.js

--- js/sorting.js
+++ js/sorting.js
@@ -1,13 +1,25 @@
-export function sortByName(data) {
-  const sortedData = [...data].sort((a,b) => 
+export function sortByName(products) {
+  const sortedData = [...products].sort((a,b) => 
     a.productName.localeCompare(b.productName)
   )
   return sortedData;
 }
 
-export function sortByPrice(data) {
-  const sortedData = [...data].sort((a,b) => 
+export function sortByPrice(products) {
+  const sortedData = [...products].sort((a,b) => 
   a.price - b.price
   )
   return sortedData;
+}
+
+// sorting.js
+
+export function getUniqueCategories(data) {
+  const categories = [...new Set(data.map(product => product.category))];
+  return categories;
+}
+
+export function filterByCategory(data, category) {
+  const filteredData = data.filter(product => product.category === category);
+  return filteredData;
 }
