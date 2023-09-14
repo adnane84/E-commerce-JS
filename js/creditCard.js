3b8e660ce085b748c81384939861cadbcc1484b2
Adnane Abid <92402125+adnane84@users.noreply.github.com>
2023-09-13 22:34:59 -0400

    Update creditCard.js

--- js/creditCard.js
+++ js/creditCard.js
@@ -1,103 +1,109 @@
+import { cartItems} from "./cart.js";
+
+// Helper function to toggle the visibility of an element by ID
+export function toggleElementVisibility(id) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.classList.toggle("hidden");
+  }
+}
+
+// Helper function to clear an error message by class
+function clearErrorMessage(className) {
+  const errorElement = document.querySelector(className);
+  if (errorElement) {
+    errorElement.textContent = "";
+  }
+}
+
 // Function to format credit card number
 export function formatCreditCard() {
-  let input = document.getElementById("credit-card");
-  let value = input.value.replace(/\D/g, ''); // Remove non-digit characters
-  let formattedValue = '';
-
-  // Insert hyphens every 4 digits
-  for (let i = 0; i < value.length; i++) {
-      if (i > 0 && i % 4 === 0) {
-          formattedValue += '-';
-      }
-      formattedValue += value.charAt(i);
-  }
-
-  // Truncate to a maximum of 20 characters
-  if (formattedValue.length > 19) {
-      formattedValue = formattedValue.substring(0, 19);
-  }
-
+  const input = document.getElementById("credit-card");
+  const value = input.value.replace(/\D/g, ''); // Remove non-digit characters
+  const formattedValue = value.replace(/(\d{4})(?=\d)/g, '$1-').substring(0, 19);
   input.value = formattedValue;
 }
 
 // Function to validate CVV
 export function validateCVV() {
   const cvvInput = document.getElementById("card-cvv");
-  const cvvError = document.querySelector(".cvv-error");
   const cvvValue = cvvInput.value.trim();
-
-  if (/^\d{3}$/.test(cvvValue)) {
-      cvvError.textContent = ""; // Clear the error message
-  } else {
-      cvvError.textContent = "CVV must be 3 digits.";
-  }
+  const cvvError = document.querySelector(".cvv-error");
+  
+  cvvError.textContent = /^\d{3}$/.test(cvvValue) ? "" : "CVV must be 3 digits.";
 }
 
 // Function to validate expiration date
 export function validateExpiration() {
   const expirationInput = document.getElementById("card-expiration");
-  const expirationError = document.querySelector(".expiration-error");
   const expirationValue = expirationInput.value.trim();
-
-  // Check if the input matches the MM/YY format
+  const expirationError = document.querySelector(".expiration-error");
   const datePattern = /^(0[1-9]|1[0-2])\/(\d{2})$/;
+
   if (!datePattern.test(expirationValue)) {
     expirationError.textContent = "Invalid format (MM/YY).";
     return;
   }
 
-  // Split the expiration date into month and year
   const [inputMonth, inputYear] = expirationValue.split('/').map(Number);
   const currentYear = new Date().getFullYear() % 100;
-  const currentMonth = new Date().getMonth() + 1; // Month is zero-based
-  console.log(currentMonth)
+  const currentMonth = new Date().getMonth() + 1;
 
   if (inputYear < currentYear || (inputYear === currentYear && inputMonth < currentMonth)) {
     expirationError.textContent = "Card has expired.";
   } else {
-    expirationError.textContent = ""; // Clear the error message
+    expirationError.textContent = "";
   }
 }
 
 // Function to validate cardholder name
 export function validateCardHolder() {
   const cardHolderInput = document.getElementById("card-holder");
-  const cardHolderError = document.querySelector(".card-holder-error");
   const cardHolderValue = cardHolderInput.value.trim();
+  const cardHolderError = document.querySelector(".card-holder-error");
 
-  if (/^[A-Za-z\s]+$/.test(cardHolderValue)) {
-      cardHolderError.textContent = ""; // Clear the error message
+  cardHolderError.textContent = /^[A-Za-z\s]+$/.test(cardHolderValue)
+    ? ""
+    : "Cardholder name can only contain letters and spaces.";
+}
+
+// Function to validate all fields and show the shipping form if all validations pass
+export function validateAndShowShippingForm() {
+  const cvvError = document.querySelector(".cvv-error");
+  const expirationError = document.querySelector(".expiration-error");
+  const cardHolderError = document.querySelector(".card-holder-error");
+
+  if (cvvError.textContent === "" && expirationError.textContent === "" && cardHolderError.textContent === "") {
+    toggleElementVisibility("shippingForm");
   } else {
-      cardHolderError.textContent = "Cardholder name can only contain letters and spaces.";
+    console.log("Please fix the errors in the form before submitting.");
   }
 }
 
-// Define the togglePaymentForm function
-export function togglePaymentForm() {
-  const paymentForm = document.getElementById("payment-form"); // Change "payment-form" to the actual ID of your payment form
-
-  // Toggle the "hidden" class on the payment form
-  paymentForm.classList.toggle("hidden");
-}
-
-// Add an event listener for the checkout button
+// Event listeners
 document.addEventListener("DOMContentLoaded", () => {
-  const checkoutButton = document.getElementById("checkoutButton"); // Change "checkout-button" to the actual ID of your checkout button
-
-  checkoutButton.addEventListener("click", () => {
-    togglePaymentForm(); // Call the togglePaymentForm function when the button is clicked
-  });
-});
-
-export function toggleShippingForm() {
-  const shippingForm = document.getElementById("shippingForm");
-  shippingForm.classList.toggle("hidden");
-}
-
-document.addEventListener("click", () => {
+  const checkoutButton = document.getElementById("checkoutButton");
   const submitBtn = document.getElementById("submit-button");
-  submitBtn.addEventListener("click", () => {
-    toggleShippingForm();
-  })
-});
+  const emptyCartMsg = document.getElementById("empty-cart-msg");
 
+  if (checkoutButton) {
+    checkoutButton.addEventListener("click", () => {
+      if (cartItems.length === 0) {
+        emptyCartMsg.innerHTML = `<h3>You cart is empty!</h3>`;
+      } else {
+        toggleElementVisibility("payment-form");
+        emptyCartMsg.innerHTML = ``;
+      }
+    });
+  }
+
+  if (submitBtn) {
+    submitBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      validateCVV();
+      validateExpiration();
+      validateCardHolder();
+      validateAndShowShippingForm();
+    });
+  }
+});
